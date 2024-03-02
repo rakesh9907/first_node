f0d80aab480bf4f216b017262fef8e4e1eb9a3c0
Nitin Verma <nitin.verma@novoed.com>
2024-03-02 13:31:33 +0530

    Add public folder

--- learn_node.js
+++ learn_node.js
@@ -1,2 +1,5 @@
 // To start Node project command
 // npm init
+
+// Here index.js is the entry point of the project which is define in package.json
+// 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 github code
