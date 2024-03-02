17417a7639f690d24b3a13f1ec7565425dfe905a
Nitin Verma <nitin.verma@novoed.com>
2024-03-02 13:53:10 +0530

    Setyp project files part 1

--- learn_node.js
+++ learn_node.js
@@ -3,3 +3,11 @@
 
 // Here index.js is the entry point of the project which is define in package.json
 // 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 github code
+
+// We have gitignore generater in market
+// In package.json we are adding "type": "module" we want to use ES6 or module technique not requeir(common js)
+
+// Here we use Nodemon for live server because without this we run server again and again
+// This is dev dependency means we only use in development not in production
+// npm i nodemon -D
+
